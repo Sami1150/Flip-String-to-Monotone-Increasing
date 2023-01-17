dd928e145d4f0563ce2f3fa22ea329c99131cefe
Sami1150 <bsef19m045@pucit.edu.pk>
2023-01-18 04:49:24 +0500

    Add project files.

---
+++ Flip String to Monotone Increasing/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int MinFlipsMonoIncr(string s)
+    {
+        int Count0 = 0; int Count1 = 0;
+        //Count Zeroes;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '0')
+            {
+                Count0++;
+            }
+        }
+        int output = Count0;
+        /*
+        *From left to right count of 0 decreases and count of 1 increases
+        */
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '0')
+            {
+                Count0--;
+            }
+            else if (s[i] == '1')
+            {
+                Count1++;
+            }
+            output = Math.Min(output, Count0 + Count1);
+        }
+        return output;
+    }
+}
